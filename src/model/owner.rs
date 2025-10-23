fbe5a56277bd080252f193bdc25d0587838856e4
David Estrella <dtestrella@ucdavis.edu>
2025-10-22 23:42:14 -0700

    Added owner method

--- src/model/owner.rs
+++ src/model/owner.rs
@@ -5,4 +5,7 @@ pub struct Owner {
     site_admin: bool,
 }
 impl Owner {
+    pub fn new(login: String, id: u64, htmlUrl: String, site_admin: bool) -> Self {
+        Self { login, id, htmlUrl, site_admin }
+    }
 }
